04b7708fe26c8fc4095fe459a6ac1f4c4e5652d9
Abhilash <abhilashmeher1234@gmail.com>
2022-04-10 11:22:40 +0530

    Added program file for UC_11

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -31,8 +31,8 @@ namespace AddressBook
             {
                 Console.WriteLine("\nEnter 1 to Add person in AddressBook");
                 Console.WriteLine("Enter 2 to Display all Contacts AdressBook");
-                Console.WriteLine("Enter 3 to Display person list and Count by by city");
-                Console.WriteLine("Enter 4 to Display person list and count by State");
+                Console.WriteLine("Enter 3 to Display the Sorted List");
+               
                 int choice=int.Parse(Console.ReadLine());
 
                 switch (choice)
@@ -44,11 +44,9 @@ namespace AddressBook
                         records.ToPrint();
                         break;
                     case 3:
-                        records.CheckByCity();
+                        records.SortContacts();
                         break;
-                    case 4:
-                        records.CheckByState();
-                        break;
+                    
                     default:
                         Console.WriteLine("Pleasee select valid input");
                         break;
